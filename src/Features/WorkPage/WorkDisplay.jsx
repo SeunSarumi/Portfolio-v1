cac21d66efac1663971847c21e09e99d6393d160
SeunSarumi <seunsarumi@gmail.com>
2026-02-07 19:34:06 +0000

    recent work pages built

--- src/Features/WorkPage/WorkDisplay.jsx
+++ src/Features/WorkPage/WorkDisplay.jsx
@@ -14,9 +14,13 @@ function WorkDisplay() {
         </div>
         <div className={styles.workTextContainer}>
           <h1>The Wild Oasis</h1>
+          <h2>Dashboard for a make belief cabin rental company</h2>
           <h3>
-            Implemented the frontend and increased usability and engagement
+            Built with : React,React Query, Context Api Supabase and styled with
+            Styled Components <br />
+            Login Details: test@test.com testtest123
           </h3>
+          {/* <p>Login Details: test@test.com testtest123 </p> */}
           <a
             className={styles.visitBtn}
             href="https://seun-wild-oasis-dash.netlify.app/dashboard"
@@ -38,9 +42,8 @@ function WorkDisplay() {
         </div>
         <div className={styles.workTextContainer}>
           <h1>Omnifood</h1>
-          <h3>
-            Implemented the frontend and increased usability and engagement
-          </h3>
+          <h2>Landing Page for make believe food app Omnifood</h2>
+          <h3>Built with : html, css and Javascript</h3>
           <a
             className={styles.visitBtn}
             href="https://omnifood-seun.netlify.app/"
@@ -56,15 +59,37 @@ function WorkDisplay() {
         <div className={styles.workImgContainer}>
           <img
             className={styles.workImg}
+            src="./images/blank-img.png"
+            alt="Blank Landing Page "
+          />
+        </div>
+        <div className={styles.workTextContainer}>
+          <h1>Blank.ng</h1>
+          <h2>
+            One of my first collaborative projects, i built the landing page for
+            my previous company along side a senior developer
+          </h2>
+          <h3>Built with : React and Css</h3>
+          <a className={styles.visitBtn} href="https://blank.ng/">
+            Vist Site
+          </a>
+        </div>
+      </div>
+      {/* work item end */}
+
+      {/* work item start */}
+      <div className={styles.workContainer}>
+        <div className={styles.workImgContainer}>
+          <img
+            className={styles.workImg}
             src="./images/natours-img.png"
-            alt="The Wild Oasis DashBoard"
+            alt="Natours image"
           />
         </div>
         <div className={styles.workTextContainer}>
           <h1>Natours</h1>
-          <h3>
-            Implemented the frontend and increased usability and engagement
-          </h3>
+          <h2>Landing Page for a make believe tourism company</h2>
+          <h3>Built with : html, SCSS and Javascript</h3>
           <a
             className={styles.visitBtn}
             href="https://seun-wild-oasis-dash.netlify.app/dashboard"
