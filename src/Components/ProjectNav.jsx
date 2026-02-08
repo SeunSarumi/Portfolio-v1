29146c5f5c9839c51b31764263f32bf0009d1523
SeunSarumi <seunsarumi@gmail.com>
2026-02-08 03:35:13 +0000

    project nav finished

--- src/Components/ProjectNav.jsx
+++ src/Components/ProjectNav.jsx
@@ -1,5 +1,34 @@
+import { Link, NavLink } from "react-router";
+import styles from "./HomeNav.module.css";
+
 function ProjectNav() {
-  return <div>Project Nav</div>;
+  return (
+    <nav className={styles.navSection}>
+      <div className={styles.navContainer}>
+        <div className={styles.firstNav}>
+          <Link to="/" className={styles.iconContainer}>
+            <img src="./images/nav-logo.png" alt="" />
+            <span className={styles.navName}>&lt;/seun sarumi&gt;</span>
+          </Link>
+          <div className={styles.btnLinks}>
+            <NavLink className={styles.btnLink} to="/projects">
+              Download CV
+            </NavLink>
+          </div>
+        </div>
+
+        <div className={styles.middleNav}>
+          <div className={styles.outerNav}>
+            <a href="https://www.linkedin.com/in/seun-sarumi-2066b8193/">
+              Linkedin
+            </a>
+            /<a href="https://github.com/SeunSarumi">Github</a>/
+            <a href="mailto:seunsarumi@gmail.com">Email</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default ProjectNav;
--- src/Features/WorkPage/RecentWork.jsx
+++ src/Features/WorkPage/RecentWork.jsx
@@ -1,5 +1,6 @@
 import HomeNav from "../../Components/HomeNav";
 import ProfieCta from "../../Components/ProfieCta";
+import ProjectNav from "../../Components/ProjectNav";
 import styles from "./RecentWork.module.css";
 import WorkDisplay from "./WorkDisplay";
 
@@ -7,7 +8,8 @@ function RecentWork() {
   return (
     <main className={styles.workSection}>
       <div className={styles.workSectionContainer}>
-        <HomeNav />
+        {/* <HomeNav /> */}
+        <ProjectNav />
         <h1 className={styles.workpageHeader}>Recent Work</h1>
         <WorkDisplay />
         <ProfieCta />
